29db767492e3ddeb2657eab6d011538c531a2373
20463702 <henkhank004@gmail.com>
2022-05-07 14:29:27 +0200

    gigalixir deps upd

--- config/prod.exs
+++ config/prod.exs
@@ -21,43 +21,9 @@ config :binckquotes, BinckquotesWeb.Endpoint,
                   "https://binckquotes.gigalixirapp.com/random",
                   "https://binckquotes.gigalixirapp.com/management",
                   "https://binckquotes.gigalixirapp.com/api/getquotes" ]
-      url: [host: "binckquotes.gigalixirapp.com"],
+      url: [host: "https://binckquotes.gigalixirapp.com", port: 4000],
       https: [
         cipher_suite: :strong,
         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
       ]
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :binckquotes, BinckquotesWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         ...,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
-#
-# The `cipher_suite` is set to `:strong` to support only the
-# latest and more secure SSL ciphers. This means old browsers
-# and clients may not be supported. You can set it to
-# `:compatible` for wider support.
-#
-# `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
-# "priv/ssl/server.key". For all supported SSL configuration
-# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-#
-# We also recommend setting `force_ssl` in your endpoint, ensuring
-# no data is ever sent via http, always redirecting to https:
-#
-#     config :binckquotes, BinckquotesWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
